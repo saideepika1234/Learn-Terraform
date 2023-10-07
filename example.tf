eefba5c68d05812d43121444590107ff82d46938
Saideepika <912183@cognizant.com>
2023-10-07 17:09:29 +0530

    Terraform basics

--- example.tf
+++ example.tf
@@ -1,9 +1,12 @@
-resource "aws_instance" "Sample" {
+resource "aws_instance" "sample" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
     Name = "Terraform"
   }
+}
 
+output "sample" {
+    value = aws_instance.sample.public_ip
 }
